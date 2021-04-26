5975a5e5542fdb39e5c783702a2c9a8b8da2cb22
cristinafaltas <faltas.cristina@iisgalvanimi.edu.it>
2021-04-26 08:31:28 +0000

    punto 5

--- Feisbuc/feisbuc-faltas/src/app/post/post.component.ts
+++ Feisbuc/feisbuc-faltas/src/app/post/post.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Post } from '../models/post.models';
 
 @Component({
   selector: 'app-post',
@@ -6,7 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post.component.css']
 })
 export class PostComponent implements OnInit {
-
+  @Input() post;
+  @Input() user;
   constructor() { }
 
   ngOnInit(): void {

bdbca2f2a149d99eaca12c8c9d082bcd324cb143
cristinafaltas <faltas.cristina@iisgalvanimi.edu.it>
2021-04-26 07:22:03 +0000

    prova

--- Feisbuc/feisbuc-faltas/src/app/app.component.ts
+++ Feisbuc/feisbuc-faltas/src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
-
+import{Post} from './models/post.models'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'feisbuc-faltas';
+  postList: Array<Post>=new Array<Post>();
+  invia(userName:HTMLInputElement, userPost:HTMLInputElement)
+  {
+    let newPost=new Post(userName.value, userPost.value);
+    this.postList.push(newPost);
+  }
 }
+
